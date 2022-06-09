697d6ca79ed69be0161763f2f85863814d5a4399
Alisson Vieira <103399107+alsn88@users.noreply.github.com>
2022-06-09 00:22:12 -0300

    Add files via upload

---
+++ theThescent.js
@@ -0,0 +1,36 @@
+javaScrip
+
+/**
+Objective:
+
+Destroy the mountains before your starship collides with one of them. For that, shoot the highest mountain on your path.
+
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game
+ * where you are given inputs (the heights of the mountains)
+ * and where you have to print an output (the index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+ **/
+
+
+// game loop
+while (true) {
+
+    var mountainHmax = -1;
+    var shot = 0;
+    for (let i = 0; i < 8; i++) {
+        const mountainH = parseInt(readline()); // represents the height of one mountain.
+        if (mountainH > mountainHmax) {
+            mountainHmax = mountainH;
+            shot = i;
+            
+        }
+    }  
+    
+
+    // Write an action using console.log()
+    // To debug: console.error('Debug messages...');
+
+   console.log(shot);     // The index of the mountain to fire on.
+
+}
